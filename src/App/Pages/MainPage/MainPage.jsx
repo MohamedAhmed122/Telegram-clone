8b2a7feec4aa39ad87a454698dc4ca322a13c870
MohamedAhmed122 <58720977+MohamedAhmed122@users.noreply.github.com>
2020-10-31 20:20:46 +0700

    Structure The App and Ready To GO0o00o0o0OOO

---
+++ src/App/Pages/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainPage() {
+    return (
+        <div className='main_page'>
+            <h1>Hello From the Main</h1>
+        </div>
+    )
+}
